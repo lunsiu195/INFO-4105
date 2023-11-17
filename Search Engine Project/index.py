ce31d1577b8397da89bc8d77b253ad8c167e7d44
harman-dhami <86544524+harman-dhami@users.noreply.github.com>
2023-11-17 13:50:35 -0800

    Delete Search Engine Project directory

--- Search Engine Project/index.py
+++
@@ -1,5 +0,0 @@
-SEARCH_KEY = "AIzaSyAqpBvsyI31skNZBf_adHwxDGtF7nWRImM"
-SEARCH_ID = "8521ec61d816a499f"
-COUNTRY = "ca"
-SEARCH_URL =  "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl" + COUNTRY
-RESULTS = 50
